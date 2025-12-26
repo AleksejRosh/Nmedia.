636cdc2545c6fe5bdb1deae38623e59888caead3
Aleksej <roshenkoaleksej@gmail.com>
2025-12-27 00:31:49 +0300

    ?

--- app/src/main/java/ru/netogy/nmedia/MainActivity.kt
+++ app/src/main/java/ru/netogy/nmedia/MainActivity.kt
@@ -21,6 +21,7 @@ class MainActivity : ComponentActivity() {
             NmediaTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
+                        name = "Nmedia!",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -30,9 +31,9 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(modifier: Modifier = Modifier) {
+fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Nmedia!",
+        text = name,
         modifier = modifier
     )
 }
@@ -41,6 +42,6 @@ fun Greeting(modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     NmediaTheme {
-        Greeting("Android")
+        Greeting("Nmedia!")
     }
 }
